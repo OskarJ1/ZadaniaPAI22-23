0e4cb9151218f0983f9d4b5530d68691e4138a1d
OskarJ1 <lolinkross59@gmail.com>
2022-09-14 15:44:31 +0200

    Komentarz

---
+++ lekcja 2/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <head>
+        <title>PAI</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <?php    
+$b=0;
+if($b == 0) {echo "Nie masz wiadomosci";}
+else {echo "Masz $b wiadomosci";}
+
+?>
+
+
+    </body>
+</html>
